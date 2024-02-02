62d98d6660c5b0a543265c8033a77691949fa4de
RAFLI1731 <rafliseptian76434@gmail.com>
2024-02-02 14:55:56 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongosee = require("mongoose");
+const dbConfig = require("./config/dbConfig");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+app.options("*", cors());
+app.use(
+  express.urlencoded({
+    enableTypes: ["json", "form"],
+    extended: true,
+  })
+);
+app.use(
+  express.json({
+    extended: true,
+  })
+);
+
+mongosee
+  .connect(dbConfig.mongoUrl, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Berhasil Konek ke mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", function (req, res) {
+  res.send("Selamat Datang");
+});
+
+app.use("/users", require("./routes/userRoutes"));
+app.use("/kategori", require("./routes/kategoriRoutes"));
+app.use("/barang", require("./routes/barangRoutes"));
+app.use("/keranjang", require("./routes/keranjangRoutes"));
+app.use("/transaksi", require("./routes/transaksiRoutes"));
+
+app.use("/gambar-barang", express.static("public/images"));
+
+const port = 3000;
+app.listen(port, () => {
+  console.log("Server Berjalan di Port");
+});
